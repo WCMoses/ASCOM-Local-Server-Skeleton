2d6b2f157a2459ea8af064f81b4353b3cb26c562
Chris moses <Chris_Moses@yahoo.com>
2019-08-18 17:10:50 -0400

    Skeleton of ASCOM Local Server Solution
    Fuly functional ASCOM local server, telescope driver and two test projects

---
+++ Driver/Skeleton2/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+[assembly: AssemblyTitle("ASCOM.Skeleton2.Telescope")]
+[assembly: AssemblyDescription("ASCOM Telescope driver for Skeleton2")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The ASCOM Initiative")]
+[assembly: AssemblyProduct("ASCOM Telescope driver for Skeleton2")]
+[assembly: AssemblyCopyright("Copyright © 2019 The ASCOM Initiative")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a2228223-b1e7-4f1b-8a07-19c1212fd723")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//
+// TODO - Set your driver's version here
+[assembly: AssemblyVersion("6.4.0.0")]
+[assembly: AssemblyFileVersion("6.4.0.0")]
